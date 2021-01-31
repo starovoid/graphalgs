35e0f3bfbbc011200933c261f76ee73200ca8a3e
PrototypeRailGun <52821033+PrototypeRailGun@users.noreply.github.com>
2021-01-31 22:42:10 +0300

    Added the floyd_warshall module.

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,7 @@ pub mod spec;
 mod shortest_distances;
 pub use shortest_distances::shortest_distances;
 
+mod floyd_warshall;
+pub use floyd_warshall::{ floyd_warshall, NegativeCycle };
+
 pub mod metrics;
