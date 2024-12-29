f120f6bd605fb8651c5787bc9cb2691d11473fb8
starovoid <prototyperailgun@gmail.com>
2024-12-29 18:04:03 +0300

    Add simple graph benches for fw & seidel

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,33 +1,12 @@
-#![allow(unused_imports)]
+mod shortest_path;
+
 use criterion::measurement::WallTime;
 use criterion::BenchmarkGroup;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use graphalgs::adj_matrix::unweighted;
 use graphalgs::elementary_circuits::elementary_circuits;
-use graphalgs::generate::{random_digraph, random_ungraph};
-use graphalgs::shortest_path::{apd, floyd_warshall, seidel, shortest_distances};
+use graphalgs::generate::random_digraph;
 use petgraph::{Directed, Graph};
-
-#[allow(unused_must_use)]
-fn run(graph: &Graph<(), f32, Directed, usize>) {
-    seidel(graph);
-}
-
-fn bench_seidel(c: &mut Criterion) {
-    let n = 100;
-    let graph = Graph::<(), f32, Directed, usize>::from_edges(
-        random_ungraph(n, n * (n - 1) / 2)
-            .unwrap()
-            .into_iter()
-            .map(|edge| (edge.0, edge.1, 1.0)),
-    );
-    c.bench_function("Seidel", |b| {
-        b.iter(|| {
-            run(&graph);
-            black_box(())
-        })
-    });
-}
+use shortest_path::*;
 
 fn elementary_circuits_helper(
     group: &mut BenchmarkGroup<WallTime>,
@@ -54,5 +33,5 @@ fn bench_elementary_circuits(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_seidel, bench_elementary_circuits);
-criterion_main!(benches);
+criterion_group!(benches, bench_elementary_circuits);
+criterion_main!(benches, shortest_path);
